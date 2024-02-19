5927ee9953187f3bbe38e51b5b53fb4bce060146
PPMELANY <melany.pintadop.est@uets.edu.ec>
2024-02-19 13:10:55 -0500

    V.02

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyABTI3bjdtC9X2u0Do4E5tq3gCyBySl_8c",
+    authDomain: "hidroponico-410d3.firebaseapp.com",
+    projectId: "hidroponico-410d3",
+    storageBucket: "hidroponico-410d3.appspot.com",
+    messagingSenderId: "209287429705",
+    appId: "1:209287429705:web:e96bd022a7b1698b150998"
+  
+}
   
 };
